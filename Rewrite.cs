674a674070de18241652a8f7f6ef2f0285088825
DeathByKorea <deathbykorea@gmail.com>
2017-08-28 00:21:44 +0100

    Even more work
    Fixed stupid parentesis that broke everything

--- Rewrite.cs
+++ Rewrite.cs
@@ -5,9 +5,9 @@ using System.Threading;
 using System.IO;
 using System.Linq;
 
-public class EssentialsPlugin { 
-    
-    #region Folder Locations
+public class EssentialsPlugin {
+
+	#region Folder Locations
 	private static string DirectoryFolder = Directory.GetCurrentDirectory() + " \\Essentials";
 	private static string SettingsFile = DirectoryFolder + "settings.txt";
 	private static string LanguageBlockFile = DirectoryFolder + "languageblock.txt";
@@ -15,7 +15,7 @@ public class EssentialsPlugin {
 
 	private static string IPListFile = "ip_list.txt";
 	private static string AdminListFile = "admin_list.txt";
-    #endregion
+	#endregion
 
 
 	#region predefining variables
@@ -25,8 +25,8 @@ public class EssentialsPlugin {
 	private static string msgUnknownCommand;
 	private static string ChatBlock;
 	private static string LanguageBlock;
-    private static string Command;
-    private static SvPlayer svplayer; 
+	private static string Command;
+	private static SvPlayer svplayer;
 
 	// Block arrays
 	private static string[] ChatBlockWords;
@@ -48,7 +48,7 @@ public class EssentialsPlugin {
 
 
 
-    /*
+	/*
 
 
 			Code below here, Don't edit unless you know what you're doing.
@@ -59,14 +59,14 @@ public class EssentialsPlugin {
 
 
 
-     #region Hooks
+	 #region Hooks
 	[Hook("SvPlayer.Initialize")]
 
-     #endregion
+	 #endregion
 
 
-    //Startup script
-    [Hook("SvNetMan.StartServerNetwork")]
+	//Startup script
+	[Hook("SvNetMan.StartServerNetwork")]
 	public static void StartServerNetwork(SvNetMan netMan)
 	{
 		if (!Directory.Exists(DirectoryFolder))
@@ -87,55 +87,86 @@ public class EssentialsPlugin {
 	}
 
 
-    //Chat Events 
+	//Chat Events
 	[Hook("SvPlayer.SvGlobalChatMessage")]
 
 	public static bool SvGlobalChatMessage(SvPlayer player, ref string message){
 
-    
-        if (message.StartsWith(cmdCommandCharacter){
-            command = message
-            
-        }
-        else { 
-            MessageLog(message);
 
-        }
-        if (LanguageBlock == true) { 
-            if (LanguageBlockWords.Any(message.ToLower().Contains)) { 
+		if (message.StartsWith(cmdCommandCharacter)){
+			command = message;
 
-            }
-        }
+		}
+		else {
+			MessageLog(message);
 
-    }
+		}
+
+		if (ChatBlock == true){
+
+			if (ChatBlockWords.Any(message.ToLower().Contains)){
+				bool blocked = BlockMessage();
+				if (blocked){
+					return false;
+				}
+				else {
+					return true;
+				}
+			}
+		}
+		if (LanguageBlock == true){
+			if (LanguageBlockWords.Any(message.ToLower().Contains)){
+				bool blocked = BlockMessage();
+				if (blocked){
+					return true;
+				}
+				else {
+					return false;
+				}
+			}
+
+		}
+	}
 
 
-    
-    //Message Logging
-    public static bool MessageLog(string message){}{
-    	if (!message.StartsWith(cmdCommandCharacter))
-		{
+	//Message Logging
+	public static void MessageLog(string message){
+		if (!message.StartsWith(cmdCommandCharacter)){
 			Debug.Log("[MESSAGE]" + player.playerData.username + ": " + message);
+		}
+	}
 
-			if (ChatBlock == true)
-			{
 
-				if (ChatBlockWords.Any(message.ToLower().Contains))
+	public static bool BlockMessage(string message){
+
+		if (ChatBlock == true) {
+
+			if (ChatBlockWords.Any(message.ToLower().Contains))
 				{
 					player.SendToSelf(Channel.Unsequenced, (byte)10, "Please don't say a blocked word, the message has been blocked.");
 					Debug.Log(player.playerData.username + " Said a word that is blocked.");
 					return true;
-				}
+				 }
 			}
-        }
-        			return false;
+			return false;
 
-    }
+			if (System.IO.File.ReadAllText(adminlist).Contains(player.playerData.username))
+				{
+					player.SendToSelf(Channel.Unsequenced, (byte)10, "Because you are staff, your message has NOT been blocked.");
+					return false;
+				}
+			else
+				{
+				player.SendToSelf(Channel.Unsequenced, (byte)10, "--------------------------------------------------------------------------------------------");
+				player.SendToSelf(Channel.Unsequenced, (byte)10, "             ?olo ingl�s! Tu mensaje ha sido bloqueado.");
+				player.SendToSelf(Channel.Unsequenced, (byte)10, "             Only English! Your message has been blocked.");
+				player.SendToSelf(Channel.Unsequenced, (byte)10, "--------------------------------------------------------------------------------------------");
+				return true;
+			}
+	}
 
-    public static void BlockMessage(string message){
 
-    }
-    
+
 
 }
 
