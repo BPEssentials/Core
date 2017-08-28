9216bd58c014e59a50c5377a30f3b0b804acdf4c
DeathByKorea <deathbykorea@gmail.com>
2017-08-29 01:11:20 +0200

    Added SSL stuff

--- Rewrite.cs
+++ Rewrite.cs
@@ -259,6 +259,7 @@ public class EssentialsPlugin {
     }
 
     public static void DownloadFile (string SrvLoc, string LocalLoc) {
+        ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
         WebClient client = new WebClient ();
         Stream stream = client.OpenRead (SrvLoc);
         StreamReader reader = new StreamReader (stream);
@@ -308,4 +309,28 @@ public class EssentialsPlugin {
             ChatBlockWords = System.IO.File.ReadAllLines (FileName);
         }
     }
+    public bool MyRemoteCertificateValidationCallback(System.Object sender,
+    X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+{
+    bool isOk = true;
+    // If there are errors in the certificate chain,
+    // look at each error to determine the cause.
+    if (sslPolicyErrors != SslPolicyErrors.None) {
+        for (int i=0; i<chain.ChainStatus.Length; i++) {
+            if (chain.ChainStatus[i].Status == X509ChainStatusFlags.RevocationStatusUnknown) {
+                continue;
+            }
+            chain.ChainPolicy.RevocationFlag = X509RevocationFlag.EntireChain;
+            chain.ChainPolicy.RevocationMode = X509RevocationMode.Online;
+            chain.ChainPolicy.UrlRetrievalTimeout = new TimeSpan (0, 1, 0);
+            chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllFlags;
+            bool chainIsValid = chain.Build ((X509Certificate2)certificate);
+            if (!chainIsValid) {
+                isOk = false;
+                break;
+            }
+        }
+    }
+    return isOk;
+}
 }
