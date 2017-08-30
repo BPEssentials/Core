ff5669c0b73c18325277a4a88b326287ac8fba0a
DeathByKorea <deathbykorea@gmail.com>
2017-08-30 17:32:42 +0100

    Fixed Mute again

--- Rewrite.cs
+++ Rewrite.cs
@@ -207,7 +207,7 @@ public class EssentialsPlugin {
 
         if (admins.Contains(player.playerData.username)) {
 
-            if (MuteListPlayers.Contains(muteuser)) {
+            if (MutePlayers.Contains(muteuser)) {
                 ReadFile(MuteListFile);
                 RemoveStringFromFile(MuteListFile, player.playerData.username);
                 player.SendToSelf(Channel.Unsequenced, (byte) 10, muteuser + " Unmuted");
