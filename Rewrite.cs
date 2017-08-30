2d4674f475c86e2f8ff65a5e04aa9deaaf9d247b
UserR00T <enverh2002@gmail.com>
2017-08-30 17:59:39 +0200

    Fixed issues and all expections to method

--- Rewrite.cs
+++ Rewrite.cs
@@ -81,32 +81,31 @@ public class EssentialsPlugin {
     public static void StartServerNetwork(SvNetMan netMan) {
 
 		try{
-        ReadFile(SettingsFile);
-		
+            ReadFile(SettingsFile);
 			Debug.Log( "[INFO] Essentials - version: " + version + " Loaded in successfully!");
-
 		} catch (Exception ex){
 			Debug.Log("[WARNING] Essentials - Settings file does not exist! Make sure to place settings.txt in the game directory! pwd: "+ Directory.GetCurrentDirectory() + "settings: " + SettingsFile);
-			Debug.Log (ex);
+			Debug.Log (ex); //remove when debugging is done
 		}
 
         if (!File.Exists(AnnouncementsFile)) {
-            Debug.Log(SetTimeStamp() + "Annoucements file doesn't exist! Please create " + AnnouncementsFile + " in the game directory");
+            Debug.Log(SetTimeStamp() + "[WARNING] Annoucements file doesn't exist! Please create " + AnnouncementsFile + " in the game directory");
         }
         Thread thread = new Thread(new ParameterizedThreadStart(AnnounceThread));
         thread.Start(netMan);
 
-        Debug.Log(SetTimeStamp() + "Announcer started successfully!");
+        Debug.Log(SetTimeStamp() + "[INFO] Announcer started successfully!");
     }
 
     //Chat Events
     [Hook("SvPlayer.SvGlobalChatMessage")]
     public static bool SvGlobalChatMessage(SvPlayer player, ref string message) {
-        // Checks if the message is a command, if not, log it
+        // Checks if player is muted, if so, cancel message
         if (MutePlayers.Contains(player.playerData.username)) {
             player.SendToSelf(Channel.Unsequenced, (byte)10, "You are muted now.");
             return true;
         }
+        // Checks if the message is a command, if not, log it
         if (message.StartsWith(cmdCommandCharacter)) {
             command = message;
             return false;
@@ -139,7 +138,6 @@ public class EssentialsPlugin {
         // Clear chat command, self and global
         if (command == cmdClearChat || command == cmdClearChat2) {
             if (command == cmdClearChat || command == cmdClearChat2) {
-
                 ClearChat(message, true);
                 return true;
             }
@@ -148,37 +146,33 @@ public class EssentialsPlugin {
                 ClearChat(message, false);
                 return true;
             }
-            // Reload command
-            if (message.StartsWith(cmdReload) || message.StartsWith(cmdReload2)) {
-                Reload(message);
-                return true;
-            }
-
-            if (message.StartsWith(cmdMute) || message.StartsWith(cmdUnMute))
-                return Mute(message);
-
-            if (message.StartsWith(cmdSay) || (message.StartsWith(cmdSay2))) {
-                return say(message);
-            }
-
-            if (message.StartsWith(cmdGodmode) || message.StartsWith(cmdGodmode2)) {
-                return godMode(message);
-            }
-
-            // Info command
-            if (message.StartsWith("/essentials") || message.StartsWith("/ess")) { 
-                essentials(message);
-            }
-            if (msgUnknownCommand) {
-                player.SendToSelf(Channel.Unsequenced, (byte) 10, "Unknown command");
-                return true;
-            } else {
-
-                return false;
-            }
-
         }
-        return false;
+        // Reload command
+        if (message.StartsWith(cmdReload) || message.StartsWith(cmdReload2)) {
+            Reload(message);
+            return true;
+        }
+
+        if (message.StartsWith(cmdMute) || message.StartsWith(cmdUnMute)) {
+            return Mute(message);
+        }
+        if (message.StartsWith(cmdSay) || (message.StartsWith(cmdSay2))) {
+            return say(message);
+        }
+
+        if (message.StartsWith(cmdGodmode) || message.StartsWith(cmdGodmode2)) {
+            return godMode(message);
+        }
+        // Info command
+        if (message.StartsWith("/essentials") || message.StartsWith("/ess")) {
+            essentials(message);
+        }
+        if (msgUnknownCommand) {
+            player.SendToSelf(Channel.Unsequenced, (byte)10, "Unknown command");
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // These are the various functions for the commands.
@@ -190,7 +184,7 @@ public class EssentialsPlugin {
                 player.svPlayer.SendToSelf(Channel.Reliable, ClPacket.GameMessage, announcements[announceIndex]);
             }
 
-            Debug.Log(SetTimeStamp() + "Announcement made...");
+            Debug.Log(SetTimeStamp() + "[INFO] Announcement made...");
 
             announceIndex += 1;
             if (announceIndex > announcements.Length - 1)
@@ -273,7 +267,8 @@ public class EssentialsPlugin {
             }
 
         } catch (Exception ex) {
-            Debug.Log(SetTimeStamp() + "[ERROR] [GODMODE] Expection: " + ex.ToString());
+
+            LogExpection(ex, "godMode");
             player.SendToSelf(Channel.Unsequenced, (byte) 10, "Unknown error. Check console for more info");
             return true;
         }
@@ -305,14 +300,17 @@ public class EssentialsPlugin {
                 player.SendToSelf(Channel.Unsequenced, (byte) 10, @"'" + arg1ClearChat + @"'" + " Is not a valid argument.");
             }
         } catch (Exception ex) {
-            Debug.Log(SetTimeStamp() + "Something went wrong while trying to make a SubString: Expection: " + ex);
-            Debug.Log(SetTimeStamp() + "Please Post the error on GitHub!");
-            Debug.Log(SetTimeStamp() + "Try reinstalling the newest version.");
+            LogExpection(ex, "ClearChat");
             player.SendToSelf(Channel.Unsequenced, (byte) 10, "Unknown error occured. Please check output_log.txt for more info.");
         }
 
     }
-
+    public static void LogExpection(Exception ex, string Sender)
+    {
+        Debug.Log(SetTimeStamp() + "[ERROR] [" + Sender + "] An unknown error occured. Expection: " + ex.ToString());
+        Debug.Log(SetTimeStamp() + "[ERROR] [" + Sender + "] Please post the error on GitHub please!");
+        Debug.Log(SetTimeStamp() + "[ERROR] [" + Sender + "] Try reinstalling the newest version.");
+    }
     public static void Reload(string message) {
         if (System.IO.File.ReadAllText(adminlist).Contains(player.playerData.username)) {
             player.SendToSelf(Channel.Unsequenced, (byte) 10, "Reloading config files...");
@@ -360,7 +358,7 @@ public class EssentialsPlugin {
             return false;
 
         } catch (Exception ex) {
-            Debug.Log(SetTimeStamp() + "[ERROR] [SAY] Expection: " + ex.ToString());
+            LogExpection(ex, "Say");
             player.SendToSelf(Channel.Unsequenced, (byte) 10, "Unknown error. Check the log for more info");
             return true;
         }
@@ -373,20 +371,24 @@ public class EssentialsPlugin {
             thread.Start(player);
         }
     }
-    private static void WriteIPToFile(object oPlayer) {
-            Thread.Sleep(500);
-            SvPlayer player = (SvPlayer) oPlayer;
-            Debug.Log(SetTimeStamp() + "[INFO] " + "[JOIN] " + player.playerData.username + " IP is: " + player.netMan.GetAddress(player.connection));
-            try {
-                if (!File.ReadAllText(iplist).Contains(player.playerData.username + ": " + player.netMan.GetAddress(player.connection))) {
-                    File.AppendAllText(iplist, player.playerData.username + ": " + player.netMan.GetAddress(player.connection) + Environment.NewLine);
-
-                }
-            } catch (Exception ex) {
-                Debug.Log(SetTimeStamp() + "[ERROR] Unknown error occured, please send the following to UserR00T:" + ex);
+    private static void WriteIPToFile(object oPlayer)
+    {
+        Thread.Sleep(500);
+        SvPlayer player = (SvPlayer)oPlayer;
+        Debug.Log(SetTimeStamp() + "[INFO] " + "[JOIN] " + player.playerData.username + " IP is: " + player.netMan.GetAddress(player.connection));
+        try
+        {
+            if (!File.ReadAllText(iplist).Contains(player.playerData.username + ": " + player.netMan.GetAddress(player.connection)))
+            {
+                File.AppendAllText(iplist, player.playerData.username + ": " + player.netMan.GetAddress(player.connection) + Environment.NewLine);
             }
-
         }
+        catch (Exception ex)
+        {
+            LogExpection(ex, "WriteIPToFile");
+        }
+
+    }
         [Hook("SvPlayer.Damage")]
     public static bool Damage(SvPlayer player, ref DamageIndex type, ref float amount, ref ShPlayer attacker, ref Collider collider) {
         try {
@@ -400,7 +402,7 @@ public class EssentialsPlugin {
                 return false;
             
         } catch (Exception ex) {
-            Debug.Log(SetTimeStamp() + "[ERROR] [GodPlugin] Expection: " + ex.ToString());
+            LogExpection(ex, "Damage");
             return false;
         }
     }
@@ -414,7 +416,7 @@ public class EssentialsPlugin {
             File.Delete(FileName);
             File.Move(tempFile, FileName);
         } catch (Exception ex) {
-            Debug.Log(SetTimeStamp() + "[ERROR] [RemoveStringFromFile] " + ex.ToString());
+            LogExpection(ex, "RemoveStringFromFile");
         }
 
     }
@@ -449,7 +451,6 @@ public class EssentialsPlugin {
         PlaceHolderText = PlaceHolderText + " ";
         return PlaceHolderText;
     } catch(Exception ex){
-		Debug.Log(ex);
 			return "[Failed] ";
 		}
 	}
