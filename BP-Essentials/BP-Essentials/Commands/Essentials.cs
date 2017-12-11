671d409f7cb84bfb90acf28923eb1cb353594113
DeathByKorea <deathbykorea@gmail.com>
2017-12-11 13:18:04 +0000

    Added Essentials.Cs (the command)

---
+++ BP-Essentials/BP-Essentials/Commands/Essentials.cs
@@ -0,0 +1,11 @@
+﻿namespace BP_Essentials.Commands {
+    public class Essentials : EssentialsCorePlugin {
+        public static bool Run(object oPlayer, string message) {
+                var player = (SvPlayer)oPlayer;
+                player.SendToSelf(Channel.Unsequenced, (byte)10, "Essentials Created by UserR00T & DeathByKorea & BP");
+                player.SendToSelf(Channel.Unsequenced, (byte)10, "Version: " + EssentialsVariablesPlugin.Version);
+            return true;
+
+        }
+    }
+}
