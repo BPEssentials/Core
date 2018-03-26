5f16529518f735a07a71c12e91bb102f179d839b
UserR00T <enverh2002@gmail.com>
2018-03-26 21:31:06 +0200

    Fixed issue with all messages being lowered

--- source/BP-Essentials/EssentialsVariables.cs
+++ source/BP-Essentials/EssentialsVariables.cs
@@ -7,7 +7,7 @@ namespace BP_Essentials
 {
     public class EssentialsVariablesPlugin : EssentialsCorePlugin
     {
-        public const string Version = "2.1.4";
+        public const string Version = "2.1.5";
 
 
         // Generic Constants
