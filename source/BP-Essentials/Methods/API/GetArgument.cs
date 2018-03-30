1a04407e6529e76f88272e905f5fda6a5c614f29
UserR00T <contact@UserR00T.com>
2018-03-30 22:42:03 +0200

    Fixed issue with GetArgument

--- source/BP-Essentials/Methods/API/GetArgument.cs
+++ source/BP-Essentials/Methods/API/GetArgument.cs
@@ -36,7 +36,7 @@ namespace BP_Essentials
                     {
                         string tmessage = message + " ";
                         string[] args = tmessage.Split(' ');
-                        return args[nr] ?? string.Empty;
+                        return nr < args.Length ? args[nr] : string.Empty;
                     }
                 }
             }
