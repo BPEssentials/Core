4ddbfc50097af4921010388f60bedb7eee5f1175
PLASMAchicken <plasmachickenmc@gmail.com>
2019-11-02 22:15:57 +0100

    Add Promote Command

---
+++ src/BPEssentials/Commands/Promote.cs
@@ -0,0 +1,37 @@
+﻿using BPCoreLib.Interfaces;
+using BPCoreLib.PlayerFactory;
+using BPEssentials.Configuration.Models.SettingsModel;
+using BPEssentials.Interfaces;
+using BrokeProtocol.API.ExtensionMethods;
+using BrokeProtocol.Entities;
+using BrokeProtocol.Utility.Networking;
+
+namespace BPEssentials.Commands
+{
+    public class Promote : ICommand
+    {
+        public bool LastArgSpaces { get; }
+
+        public ILogger Logger { get; set; }
+
+        public Settings Settings { get; set; }
+
+        public ExtendedPlayerFactory PlayerFactory { get; set; }
+
+        public void Invoke(ShPlayer player, ShPlayer target)
+        {
+            if (target.job.info.rankItems.Length == 0)  // Check if Target's Job even has Ranks
+            {
+                player.SendChatMessage($"{target.username.SanitizeString()}'s Job does not have ranks!");
+                return;
+            }
+            if (target.rank == target.job.info.rankItems.Length - 1)    // Check if Target is max rank
+            {
+                player.SendChatMessage($"{target.username.SanitizeString()} already has the highest rank!");
+                return;
+            }
+            target.svPlayer.Reward(target.maxExperience - target.experience + 1, 0);
+            player.SendChatMessage($"Promoted {target.username.SanitizeString()} to rank {target.rank + 1}.");
+        }
+    }
+}
