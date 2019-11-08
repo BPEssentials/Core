0aeb0841b499b820c419e6be3dc3b809df230737
PLASMAchicken <plasmachickenmc@gmail.com>
2019-11-09 00:43:07 +0100

    Create Launch.cs

---
+++ src/BPEssentials/Commands/Launch.cs
@@ -0,0 +1,30 @@
+﻿using BPCoreLib.Interfaces;
+using BPCoreLib.PlayerFactory;
+using BPEssentials.Configuration.Models.SettingsModel;
+using BPEssentials.ExtendedPlayer;
+using BPEssentials.Interfaces;
+using BrokeProtocol.API.ExtensionMethods;
+using BrokeProtocol.Entities;
+using BrokeProtocol.Utility.Networking;
+
+namespace BPEssentials.Commands
+{
+    public class Launch : ICommand
+    {
+        public bool LastArgSpaces { get; }
+
+        public ILogger Logger { get; set; }
+
+        public Settings Settings { get; set; }
+
+        public ExtendedPlayerFactory<PlayerItem> PlayerFactory { get; set; }
+
+        public void Invoke(ShPlayer player, ShPlayer target)
+        {
+            target.svPlayer.SvForce(new UnityEngine.Vector3(0f, 6500f, 0f));
+            target.SendChatMessage($"Off you go!");
+            player.SendChatMessage($"You've launched {target.username.SanitizeString()} into space!");
+
+        }
+    }
+}
