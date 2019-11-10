bbf8b9c2c5bc31e37ec8b8a96a1edabb5c37b550
PLASMAchicken <plasmachickenmc@gmail.com>
2019-11-10 15:11:38 +0100

    Create Back.cs

---
+++ src/BPEssentials/Commands/Back.cs
@@ -0,0 +1,33 @@
+﻿using BPCoreLib.Interfaces;
+using BPCoreLib.PlayerFactory;
+using BPEssentials.Configuration.Models.SettingsModel;
+using BPEssentials.ExtendedPlayer;
+using BPEssentials.ExtensionMethods;
+using BPEssentials.Interfaces;
+using BrokeProtocol.Entities;
+
+namespace BPEssentials.Commands
+{
+    public class Back : ICommand
+    {
+        public bool LastArgSpaces { get; }
+
+        public ILogger Logger { get; set; }
+
+        public Settings Settings { get; set; }
+
+        public ExtendedPlayerFactory<PlayerItem> PlayerFactory { get; set; }
+
+        public void Invoke(ShPlayer player, ShPlayer target)
+        {
+            var lastLocation = target.GetExtendedPlayer().lastLocation;
+            if (!lastLocation.HasPositionSet())
+            {
+                player.SendChatMessage($"There is no location to teleport to.");
+                return;
+            }
+            target.GetExtendedPlayer().ResetAndSavePosition(lastLocation.Position, lastLocation.Rotation, lastLocation.PlaceIndex);
+            target.SendChatMessage("You've been teleported to your last known location.");
+        }
+    }
+}
