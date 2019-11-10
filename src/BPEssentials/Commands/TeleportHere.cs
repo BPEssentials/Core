a70b1aff96437c643f9d1c05595680978f38cc67
PLASMAchicken <plasmachickenmc@gmail.com>
2019-11-10 15:19:05 +0100

    Create TeleportHere.cs

---
+++ src/BPEssentials/Commands/TeleportHere.cs
@@ -0,0 +1,29 @@
+﻿using BPCoreLib.Interfaces;
+using BPCoreLib.PlayerFactory;
+using BPEssentials.Configuration.Models.SettingsModel;
+using BPEssentials.ExtendedPlayer;
+using BPEssentials.ExtensionMethods;
+using BPEssentials.Interfaces;
+using BrokeProtocol.API.ExtensionMethods;
+using BrokeProtocol.Entities;
+
+namespace BPEssentials.Commands
+{
+    public class TeleportHere : ICommand
+    {
+        public bool LastArgSpaces { get; }
+
+        public ILogger Logger { get; set; }
+
+        public Settings Settings { get; set; }
+
+        public ExtendedPlayerFactory<PlayerItem> PlayerFactory { get; set; }
+
+        public void Invoke(ShPlayer player, ShPlayer target)
+        {
+            target.GetExtendedPlayer().ResetAndSavePosition(player);
+            player.SendChatMessage($"You've teleported {target.username.SanitizeString()} to you.");
+            target.SendChatMessage($"{player.username.SanitizeString()} has teleported you to him.");
+        }
+    }
+}
