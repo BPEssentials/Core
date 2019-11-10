72be62e62a58fbabd1a7703c888c590bdd123b0c
PLASMAchicken <plasmachickenmc@gmail.com>
2019-11-10 15:10:49 +0100

    Add PlayerItem#lastLocation

--- src/BPEssentials/ExtendedPlayer/ExtendedPlayer.cs
+++ src/BPEssentials/ExtendedPlayer/ExtendedPlayer.cs
@@ -1,5 +1,6 @@
 ﻿using BrokeProtocol.API.ExtensionMethods;
 using BrokeProtocol.Entities;
+using UnityEngine;
 
 namespace BPEssentials.ExtendedPlayer
 {
@@ -21,6 +22,8 @@ namespace BPEssentials.ExtendedPlayer
 
         public ShPlayer TpaUser { get; set; }
 
+        public LastLocation lastLocation { get; set; } = new LastLocation();
+
         public void SendPmMessage(ShPlayer target, string message)
         {
             Client.SendChatMessage($"[PM] {target.username.SanitizeString()} > {message.SanitizeString()}");
@@ -37,6 +40,35 @@ namespace BPEssentials.ExtendedPlayer
             Disabled,
             Global,
             StaffChat
+        }
+
+        public class LastLocation
+        {
+            public Vector3 Position { get; private set; }
+            public Quaternion Rotation { get; private set; }
+            public int PlaceIndex { get; private set; }
+            public void Update(Vector3 position, Quaternion rotation, int index)
+            {
+                Position = position;
+                Rotation = rotation;
+                PlaceIndex = index;
+            }
+            public void Update(ShPlayer player)
+            {
+                Update(player.GetPosition(), player.GetRotation(), player.GetPlaceIndex());
+            }
+            public bool HasPositionSet()
+            {
+                return Position != default(Vector3) && Rotation != default(Quaternion);
+            }
+            public LastLocation(Vector3 position, Quaternion rotation, int index)
+            {
+                Update(position, rotation, index);
+            }
+            public LastLocation()
+            {
+                
+            }
         }
     }
 }
---
+++ src/BPEssentials/ExtensionMethods/PlayerItem.cs
@@ -0,0 +1,28 @@
+﻿using BPEssentials.ExtendedPlayer;
+using BrokeProtocol.Entities;
+using UnityEngine;
+
+namespace BPEssentials.ExtensionMethods
+{
+    public static class ExtensionPlayerItem
+    {
+        public static void SavePosition(this PlayerItem player)
+        {
+            player.SavePosition(player.Client.GetPosition(), player.Client.GetRotation(), player.Client.GetPlaceIndex());
+        }
+        public static void SavePosition(this PlayerItem player, Vector3 position, Quaternion rotation, int index)
+        {
+            player.lastLocation.Update(position, rotation, index);
+        }
+        public static void ResetAndSavePosition(this PlayerItem player, Vector3 position, Quaternion rotation, int index)
+        {
+            player.SavePosition();
+            player.Client.svPlayer.SvReset(position, rotation, index);
+        }
+        public static void ResetAndSavePosition(this PlayerItem player, ShPlayer targetPlayer)
+        {
+            player.SavePosition();
+            player.Client.svPlayer.SvReset(targetPlayer.GetPosition(), targetPlayer.GetRotation(), targetPlayer.GetPlaceIndex());
+        }
+    }
+}
