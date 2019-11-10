01de28509826b3e4d273a42010b6b5f1bdb6cc9b
UserR00T <contact@UserR00T.com>
2019-11-10 16:24:16 +0100

    Removed extra spaces

--- src/BPEssentials/Commands/Promote.cs
+++ src/BPEssentials/Commands/Promote.cs
@@ -20,13 +20,13 @@ namespace BPEssentials.Commands
 
         public void Invoke(ShPlayer player, ShPlayer target = null)
         {
-            if (target.job.info.rankItems.Length == 0)  // Check if Target's Job even has Ranks
             target = target ?? player;
+            if (target.job.info.rankItems.Length == 0) // Check if Target's Job even has Ranks
             {
                 player.SendChatMessage($"{target.username.SanitizeString()}'s Job does not have ranks!");
                 return;
             }
-            if (target.rank == target.job.info.rankItems.Length - 1)    // Check if Target is max rank
+            if (target.rank == target.job.info.rankItems.Length - 1) // Check if Target is max rank
             {
                 player.SendChatMessage($"{target.username.SanitizeString()} already has the highest rank!");
                 return;
