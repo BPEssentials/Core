a5f78530ae74a8bd6800115836b8be552dbb169e
PLASMAchicken <plasmachickenmc@gmail.com>
2019-11-10 15:19:28 +0100

    Create Teleport.cs

---
+++ src/BPEssentials/Commands/Teleport.cs
@@ -0,0 +1,28 @@
+﻿using BPCoreLib.Interfaces;
+using BPCoreLib.PlayerFactory;
+using BPEssentials.Configuration.Models.SettingsModel;
+using BPEssentials.ExtendedPlayer;
+using BPEssentials.ExtensionMethods;
+using BPEssentials.Interfaces;
+using BrokeProtocol.API.ExtensionMethods;
+using BrokeProtocol.Entities;
+
+namespace BPEssentials.Commands
+{
+    public class Teleport : ICommand
+    {
+        public bool LastArgSpaces { get; }
+
+        public ILogger Logger { get; set; }
+
+        public Settings Settings { get; set; }
+
+        public ExtendedPlayerFactory<PlayerItem> PlayerFactory { get; set; }
+
+        public void Invoke(ShPlayer player, ShPlayer target)
+        {
+            player.GetExtendedPlayer().ResetAndSavePosition(target);
+            player.SendChatMessage($"You've been teleported to {target.username.SanitizeString()}.");
+        }
+    }
+}
