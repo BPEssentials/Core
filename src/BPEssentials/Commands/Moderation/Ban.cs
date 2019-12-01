8bbda0a3dd3708d2d2b027983b58559f758a76e8
PLASMAchicken <plasmachickenmc@gmail.com>
2019-12-01 17:55:30 +0100

    Add Ban.cs

---
+++ src/BPEssentials/Commands/Moderation/Ban.cs
@@ -0,0 +1,18 @@
+﻿using BPEssentials.Abstractions;
+using BPEssentials.ExtensionMethods;
+using BrokeProtocol.API.ExtensionMethods;
+using BrokeProtocol.Entities;
+using BrokeProtocol.Utility.Networking;
+
+namespace BPEssentials.Commands
+{
+    public class Ban : Command
+    {
+        public void Invoke(ShPlayer player, ShPlayer target, string reason = "none")
+        {
+            Util.SendToAllEnabledChatT("all_ban", player.username.SanitizeString(), target.username.SanitizeString(), reason.SanitizeString());
+            player.TS("player_banned", target.username.SanitizeString(), reason.SanitizeString());
+            player.svPlayer.SvBan(target.ID, reason);
+        }
+    }
+}
