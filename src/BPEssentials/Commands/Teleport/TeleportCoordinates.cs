f5cab0000f12a77f06bef421952a66dc383355b9
PLASMAchicken <plasmachickenmc@gmail.com>
2019-12-01 17:01:47 +0100

    Fix TeleportCoordinates

---
+++ src/BPEssentials/Commands/Teleport/TeleportCoordinates.cs
@@ -0,0 +1,21 @@
+﻿using BPEssentials.Abstractions;
+using BrokeProtocol.Entities;
+using BPEssentials.ExtensionMethods;
+using UnityEngine;
+
+namespace BPEssentials.Commands
+{
+    public class TeleportCoordinates : Command
+    {
+        public void Invoke(ShPlayer player, float x, float y, float z, int placeIndex = 0)
+        {
+            if(placeIndex < 0 || placeIndex > (Core.Instance.SvManager.fixedPlaces.Count + Core.Instance.SvManager.apartments.Count))
+            {
+                player.TS("TeleportCoordinates_invalidPlaceIndex", placeIndex.ToString());
+                return;
+            }
+            player.GetExtendedPlayer().ResetAndSavePosition(new Vector3(x, y, z), player.GetRotation(), placeIndex);
+            player.TS("TeleportCoordinates", x.ToString(), y.ToString(), z.ToString(), placeIndex.ToString());
+        }
+    }
+}
