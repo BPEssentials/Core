78959e6e831bb288b4642a45a7af10ae6ef2a0f5
PLASMAchicken <plasmachickenmc@gmail.com>
2019-12-02 16:46:14 +0100

    Create EntityHandler.cs

---
+++ src/BPEssentials/EntityHandler.cs
@@ -0,0 +1,33 @@
+﻿using BrokeProtocol.Entities;
+using BrokeProtocol.Managers;
+using System.Collections.Generic;
+
+namespace BPEssentials
+{
+    public class EntityHandler
+    {
+
+        public Dictionary<string, ShItem> Items { get; set; } = new Dictionary<string, ShItem>();
+
+        public Dictionary<string, ShTransport> Vehicles { get; set; } = new Dictionary<string, ShTransport>();
+
+        public void LoadEntities()
+        {
+            foreach (var entity in SceneManager.Instance.entityCollection)
+            {
+                if (entity.Value == null)
+                {
+                    continue;
+                }
+                if (entity.Value is ShItem)
+                {
+                    Items.Add(entity.Value.name, entity.Value as ShItem);
+                }
+                if (entity.Value is ShTransport && !(entity.Value is ShParachute))
+                {
+                    Vehicles.Add(entity.Value.name, entity.Value as ShTransport);
+                }
+            }
+        }
+    }
+}
