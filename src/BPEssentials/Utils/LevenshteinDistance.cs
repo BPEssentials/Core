ded3eb598fbe3ac6bef43cb1fcba4978afb344cf
PLASMAchicken <plasmachickenmc@gmail.com>
2019-12-02 16:14:32 +0100

    codacy fix

--- src/BPEssentials/Utils/LevenshteinDistance.cs
+++ src/BPEssentials/Utils/LevenshteinDistance.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace BPEssentials.Utils
 {
@@ -6,15 +7,26 @@ namespace BPEssentials.Utils
     {
         public static int ComputeLevenshteinDistance(string source, string target)
         {
-            if (source == target) return 0;
-            if (source.Length == 0) return target.Length;
-            if (target.Length == 0) return source.Length;
-            source = source.ToLower();
-            target = target.ToLower();
+            if (source == target)
+            {
+                return 0;
+            }
+            if (source.Length == 0)
+            {
+                return target.Length;
+            }
+            if (target.Length == 0)
+            {
+                return source.Length;
+            }
+            source = source.ToLower(CultureInfo.InvariantCulture);
+            target = target.ToLower(CultureInfo.InvariantCulture);
             int[] v0 = new int[target.Length + 1];
             int[] v1 = new int[target.Length + 1];
             for (int i = 0; i < v0.Length; i++)
+            {
                 v0[i] = i;
+            }
 
             for (int i = 0; i < source.Length; i++)
             {
@@ -25,7 +37,9 @@ namespace BPEssentials.Utils
                     v1[j + 1] = Math.Min(v1[j] + 1, Math.Min(v0[j + 1] + 1, v0[j] + cost));
                 }
                 for (int j = 0; j < v0.Length; j++)
+                {
                     v0[j] = v1[j];
+                }
             }
 
             return v1[target.Length];
@@ -33,9 +47,9 @@ namespace BPEssentials.Utils
 
         public static double CalculateSimilarity(string source, string target)
         {
-            if ((source == null) || (target == null)) return 0.0;
-            if ((source.Length == 0) || (target.Length == 0)) return 0.0;
-            if (source == target) return 1.0;
+            if ((source == null) || (target == null)) { return 0.0; }
+            if ((source.Length == 0) || (target.Length == 0)) { return 0.0; }
+            if (source == target) {return 1.0; }
 
             int stepsToSame = ComputeLevenshteinDistance(source, target);
             return 1.0 - ((double)stepsToSame / (double)Math.Max(source.Length, target.Length));
