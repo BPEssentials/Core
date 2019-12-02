8de5eca679dbb3ad67b35086d93c21175cbf2ae5
PLASMAchicken <plasmachickenmc@gmail.com>
2019-12-02 15:41:37 +0100

    Split Util.cs into diffrent files

---
+++ src/BPEssentials/Utils/ReloadUtil.cs
@@ -0,0 +1,11 @@
+﻿namespace BPEssentials.Utils
+{
+    public static class ReloadUtil
+    {
+        public static void ReloadFiles()
+        {
+            Core.Instance.ReadConfigurationFiles();
+            Core.Instance.RegisterCustomCommands();
+        }
+    }
+}
