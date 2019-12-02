c49dbce007266fadeab2da2ae8505a9efc4feb2c
PLASMAchicken <plasmachickenmc@gmail.com>
2019-12-02 16:48:14 +0100

    Create Give.cs

---
+++ src/BPEssentials/Commands/Player/Inventory/Give.cs
@@ -0,0 +1,21 @@
+﻿using BPEssentials.Abstractions;
+using BPEssentials.ExtensionMethods;
+using BPEssentials.Utils;
+using BrokeProtocol.API.ExtensionMethods;
+using BrokeProtocol.Entities;
+using BrokeProtocol.Utility;
+using System.Linq;
+
+namespace BPEssentials.Commands
+{
+    public class Give : Command
+    {
+        public void Invoke(ShPlayer player, string name, int amount = 1, ShPlayer target = null)
+        {
+            target = target ?? player;
+            var item = Core.Instance.EntityHandler.Items.OrderByDescending(x => LevenshteinDistance.CalculateSimilarity(x.Key, name)).FirstOrDefault();
+            target.TransferItem(DeltaInv.AddToMe, item.Value.index, amount, true);
+            player.TS("player_give", target.username.SanitizeString(), name, amount.ToString());
+        }
+    }
+}
