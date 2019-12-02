6cb527cf74df8bae998407fe107b835037506eae
PLASMAchicken <plasmachickenmc@gmail.com>
2019-12-02 16:49:11 +0100

    Create RepairVehicle.cs

---
+++ src/BPEssentials/Commands/Vehicles/RepairVehicle.cs
@@ -0,0 +1,24 @@
+﻿using BPEssentials.Abstractions;
+using BPEssentials.ExtensionMethods;
+using BPEssentials.Utils;
+using BrokeProtocol.Entities;
+using System.Linq;
+using UnityEngine;
+
+namespace BPEssentials.Commands
+{
+    public class RepairVehicle : Command
+    {
+        public void Invoke(ShPlayer player, string name)
+        {
+            var vehicle = player.curMount as ShTransport;
+            if (vehicle == null)
+            {
+                player.TS("player_notInVehicle");
+                return;
+            }
+            vehicle.RestoreStats();
+            player.TS("player_vehicle_repaired", vehicle.name);
+        }
+    }
+}
