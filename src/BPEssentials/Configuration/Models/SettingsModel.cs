a120118f22a743572390c483b64a0e62f437b7cc
PLASMAchicken <plasmachickenmc@gmail.com>
2019-12-10 23:48:45 +0100

    reorder so it matches the order of setings.json

--- src/BPEssentials/Configuration/Models/SettingsModel.cs
+++ src/BPEssentials/Configuration/Models/SettingsModel.cs
@@ -6,9 +6,9 @@ namespace BPEssentials.Configuration.Models.SettingsModel
     {
         public General General { get; set; }
 
-        public Messages Messages { get; set; }
-
         public List<string> Announcements { get; set; }
+
+        public Messages Messages { get; set; }
 
         public FunctionUI FunctionUI { get; set; }
 
