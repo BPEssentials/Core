f841aa7738644595d2ce0a14b7da8711f77cfa88
PLASMAchicken <plasmachickenmc@gmail.com>
2019-12-15 01:37:22 +0100

    Better Warn List, now using ServerInfo

--- src/BPEssentials/Commands/Warns/WarnList.cs
+++ src/BPEssentials/Commands/Warns/WarnList.cs
@@ -3,7 +3,9 @@ using BPEssentials.ExtensionMethods;
 using BPEssentials.ExtensionMethods.Warns;
 using BrokeProtocol.API.ExtensionMethods;
 using BrokeProtocol.Entities;
+using BrokeProtocol.Utility.Networking;
 using System.Linq;
+using System.Text;
 
 namespace BPEssentials.Commands
 {
@@ -11,14 +13,29 @@ namespace BPEssentials.Commands
     {
         public void Invoke(ShPlayer player, ShPlayer target = null)
         {
-            if(target != null && !player.svPlayer.HasPermission($"{Core.Instance.Info.GroupNamespace}.{nameof(WarnList)}.viewotherplayers"))
+            if (target != null && !player.svPlayer.HasPermission($"{Core.Instance.Info.GroupNamespace}.{nameof(WarnList)}.viewotherplayers"))
             {
                 player.TS("warns_noPermission_viewOtherPlayers");
                 return;
             }
             target = target ?? player;
             var warns = target.GetWarns().Select(warn => warn.ToString()).ToArray();
-            player.TS("warns", target.username.SanitizeString(), warns.Length.ToString(), (warns.Length == 0 ? "none" : string.Join(", ", warns)));
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine(player.T("warns_for", target.username.SanitizeString()));
+            if (warns.Length == 0)
+            {
+                stringBuilder.AppendLine(player.T("none"));
+            }
+            else
+            {
+                stringBuilder.AppendLine(player.T("warns_count", warns.Length.ToString()));
+                stringBuilder.AppendLine();
+                for (int i = 0; i < warns.Length; i++)
+                {
+                    stringBuilder.AppendLine($"{i + 1} - {warns[i]}");
+                }
+            }
+            player.svPlayer.Send(SvSendType.Self, Channel.Reliable, ClPacket.ServerInfo, stringBuilder.ToString());
         }
     }
 }
