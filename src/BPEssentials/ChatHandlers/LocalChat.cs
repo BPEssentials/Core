fa31fde77ab20dfbad4eed86a0b97cb95264d3c6
PLASMAchicken <plasmachickenmc@gmail.com>
2020-03-07 11:39:49 +0100

    Reflect new GameSource Updates

--- src/BPEssentials/ChatHandlers/LocalChat.cs
+++ src/BPEssentials/ChatHandlers/LocalChat.cs
@@ -1,25 +1,34 @@
 ﻿using BrokeProtocol.API;
-using BrokeProtocol.API.ExtensionMethods;
-using BrokeProtocol.API.Types;
 using BrokeProtocol.Entities;
+using BrokeProtocol.Utility;
 using System;
+using BPEssentials.ExtensionMethods;
+using BrokeProtocol.Utility.Networking;
+using BPEssentials.Utils;
 
 namespace BPEssentials.ChatHandlers
 {
     public class LocalChat : IScript
     {
-        public LocalChat()
-        {
-            GameSourceHandler.Add(BrokeProtocol.API.Events.Player.OnLocalChatMessage, new Action<ShPlayer, string>(OnEvent));
-        }
-
+        [Target(GameSourceEvent.PlayerLocalChatMessage, ExecutionMode.Override)]
         public void OnEvent(ShPlayer player, string message)
         {
-            if (message.StartsWith(CommandHandler.Prefix))
+            if (player.manager.svManager.chatted.OverLimit(player))
             {
                 return;
             }
-            Core.Instance.Logger.LogInfo($"[LOCAL] {player.username.SanitizeString()}: {message.SanitizeString()}");
+
+            Core.Instance.Logger.LogInfo($"[LOCAL] {player.username.CleanerMessage()}: {message.CleanerMessage()}");
+
+
+            if (CommandHandler.OnEvent(player, message)) // 'true' if message starts with command prefix
+            {
+                return;
+            }
+
+            player.manager.svManager.chatted.Add(player);
+
+            player.svPlayer.Send(SvSendType.LocalOthers, Channel.Unsequenced, ClPacket.LocalChatMessage, player.ID, ChatUtils.FormatMessage(player, message, "localformat"));
         }
     }
 }
