bd4adb23ca576686c0b50245ec93c22b5e82c822
PLASMAchicken <plasmachickenmc@gmail.com>
2020-03-07 12:07:48 +0100

    Make v1.05 ready

--- src/BPEssentials/Commands/Warps/WarpList.cs
+++ src/BPEssentials/Commands/Warps/WarpList.cs
@@ -9,7 +9,7 @@ namespace BPEssentials.Commands
     {
         public void Invoke(ShPlayer player)
         {
-            var warps = Core.Instance.WarpHandler.List.Where(x => !x.Disabled && player.svPlayer.HasPermission($"{Core.Instance.Info.GroupNamespace}.{Core.Instance.WarpHandler.Name}.{x.Name}")).Select(n => n.Name + $"{(n.Price != 0 ? $" ({n.Price})" : "")}").ToArray();
+            var warps = Core.Instance.WarpHandler.List.Where(x => !x.Disabled && player.HasPermission($"{Core.Instance.Info.GroupNamespace}.{Core.Instance.WarpHandler.Name}.{x.Name}")).Select(n => n.Name + $"{(n.Price != 0 ? $" ({n.Price})" : "")}").ToArray();
             player.TS("warps", warps.Length.ToString(), (warps.Length == 0 ? "none" : string.Join(", ", warps)));
         }
     }
