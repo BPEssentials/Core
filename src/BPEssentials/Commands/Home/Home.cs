d3d200db4f2c2cd6f9cc132a5c98b0dc116a86e9
Ido <benhaim.ido@gmail.com>
2020-03-11 21:09:51 -0400

    Add /home
    Well I just added /home
    To use just do /home HOMENUMBER
    eg. /home 1

---
+++ src/BPEssentials/Commands/Home/Home.cs
@@ -0,0 +1,33 @@
+﻿using BPEssentials.Abstractions;
+using BPEssentials.ExtensionMethods;
+using BrokeProtocol.Entities;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BPEssentials.Commands
+{
+    public class Home : Command
+    {
+        public void Invoke(ShPlayer player, int homeNumber)
+        {
+            //make the apartments start at 1 (eg. /home 1 would point to apartments[0])
+            homeNumber--;
+            List<ShApartment> apartments = new List<ShApartment>(player.ownedApartments.Keys);
+            try
+            {
+                player.GetExtendedPlayer().ResetAndSavePosition(apartments[homeNumber].GetPosition + new Vector3(1, 1, 1), apartments[homeNumber].GetRotation, apartments[homeNumber].GetPlaceIndex);
+            }
+            catch(System.IndexOutOfRangeException e)
+            {
+                player.TS("You_only_own", apartments.Count.ToString()) ;
+            }
+                }
+
+
+
+
+
+
+    }
+}
