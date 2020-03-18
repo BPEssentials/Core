eb86fa6e10cab0f0ebe314b8a3cf364e49b188d1
Ido <benhaim.ido@gmail.com>
2020-03-17 22:41:49 -0400

    Pay
    Using /pay you can pay another user...

---
+++ src/BPEssentials/Commands/Pay/Pay.cs
@@ -0,0 +1,26 @@
+﻿using BPEssentials.Abstractions;
+using BPEssentials.ExtensionMethods;
+using BPEssentials.Utils;
+
+using BrokeProtocol.Entities;
+using BrokeProtocol.Utility;
+using System.Linq;
+
+
+namespace BPEssentials.Commands
+{
+    public class Pay : Command
+    {
+        public void Invoke(ShPlayer player, ShPlayer target, int amount=1)
+        {
+            var item = Core.Instance.EntityHandler.Items.FirstOrDefault(x => x.Key == "Money");
+            if (player.MyItemCount(item.Value.index) < amount)
+            {
+                player.TS("you_dont_own", amount.ToString());
+                return;
+            }
+            player.TransferItem(DeltaInv.RemoveFromMe, item.Value.index, amount, true);
+            target.TransferItem(DeltaInv.AddToMe, item.Value.index, amount, true);
+        }
+    }
+}
