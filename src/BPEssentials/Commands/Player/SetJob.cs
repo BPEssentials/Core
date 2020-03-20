819ed278095e74ac2de215ffad79eaa0e50503ef
Ido <benhaim.ido@gmail.com>
2020-03-19 23:02:33 -0400

    Setjob command
    The jobs:
            Citizen
            Criminal
            Prisoner
            Police
            Paramedic
            Firefighter
            GangsterRed
            GangsterGreen
            GangsterBlue
            Mayor
            DeliveryDriver
            TaxiDriver
            SpecOps

---
+++ src/BPEssentials/Commands/Player/SetJob.cs
@@ -0,0 +1,22 @@
+﻿using BPEssentials.Abstractions;
+using BPEssentials.ExtensionMethods;
+using BrokeProtocol.Entities;
+using BrokeProtocol.Utility;
+using BrokeProtocol.Required;
+
+namespace BPEssentials.Commands
+{
+    public class SetJob : Command
+    {
+        public void Invoke(ShPlayer player, JobIndex job, ShPlayer target = null)
+        {
+            
+            target = target ?? player;
+            target.svPlayer.SvTrySetJob(job, true, false);
+            player.TS("job_set", target.username.CleanerMessage(), job.ToString());
+            if (target != player)
+                target.TS("new_job", player.username.CleanerMessage(), job.ToString());
+        }
+
+    }
+}
