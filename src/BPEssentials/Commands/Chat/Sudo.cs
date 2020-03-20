88a35f5c9c85338bc3581fa38135b8af66114b6d
Ido <benhaim.ido@gmail.com>
2020-03-20 16:22:11 -0400

    Sudo

---
+++ src/BPEssentials/Commands/Chat/Sudo.cs
@@ -0,0 +1,15 @@
+﻿using BrokeProtocol.Entities;
+using BPEssentials.Utils;
+using BPEssentials.Abstractions;
+
+namespace BPEssentials.Commands
+{
+    public class Sudo : Command
+    {
+        public void Invoke(ShPlayer player, ShPlayer target, string message)
+        {
+            Core.Instance.Logger.LogInfo($"[GLOBAL] {target.username}: {message}");
+            ChatUtils.SendToAllEnabledChat(ChatUtils.FormatMessage(target, message), false);
+        }
+    }
+}
