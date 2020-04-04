a8208bb72ae6ff715b04f969af075d7bba78466c
PLASMAchicken <plasmachickenmc@gmail.com>
2020-04-04 13:08:56 +0200

    3.0.1

--- src/BPEssentials/Commands/Moderation/Jail.cs
+++ src/BPEssentials/Commands/Moderation/Jail.cs
@@ -8,29 +8,29 @@ using System.Linq;
 
 namespace BPEssentials.Commands
 {
-	public class Jail : Command
-	{
-		public void Invoke(ShPlayer player, ShPlayer target, float timeInSeconds)
-		{
-			var jail = target.manager.jails.FirstOrDefault();
-			if (jail == null)
-			{
-				return;
-			}
-			if (target.IsDead || target.job is Prisoner)
-			{
-				return;
-			}
-			var getPositionT = jail.GetPositionT;
-			target.svPlayer.SvTrySetJob(JobIndex.Prisoner, true, false);
-			target.GetExtendedPlayer().ResetAndSavePosition(getPositionT.position, getPositionT.rotation, jail.GetPlaceIndex);
-			target.svPlayer.SvClearCrimes();
-			target.RemoveItemsJail();
-			target.StartCoroutine(target.svPlayer.JailTimer(timeInSeconds));
-			target.svPlayer.Send(SvSendType.Self, Channel.Reliable, ClPacket.ShowTimer, timeInSeconds);
-			player.TS("player_jail", target.username.CleanerMessage(), timeInSeconds);
-			target.TS("target_jail", player.username.CleanerMessage(), timeInSeconds);
-			}
-		}
-	}
+    public class Jail : Command
+    {
+        public void Invoke(ShPlayer player, ShPlayer target, float timeInSeconds)
+        {
+            var jail = target.manager.jails.FirstOrDefault();
+            if (jail == null)
+            {
+                return;
+            }
+            if (target.IsDead || target.job is Prisoner)
+            {
+                return;
+            }
+            var getPositionT = jail.GetPositionT;
+            target.svPlayer.SvTrySetJob(JobIndex.Prisoner, true, false);
+            target.GetExtendedPlayer().ResetAndSavePosition(getPositionT.position, getPositionT.rotation, jail.GetPlaceIndex);
+            target.svPlayer.SvClearCrimes();
+            target.RemoveItemsJail();
+            target.StartCoroutine(target.svPlayer.JailTimer(timeInSeconds));
+            target.svPlayer.Send(SvSendType.Self, Channel.Reliable, ClPacket.ShowTimer, timeInSeconds);
+            player.TS("player_jail", target.username.CleanerMessage(), timeInSeconds);
+            target.TS("target_jail", player.username.CleanerMessage(), timeInSeconds);
+        }
+    }
 }
+
