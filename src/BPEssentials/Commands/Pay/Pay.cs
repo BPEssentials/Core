9537bb5a8f0b4a800aa5a2bb2a370763675df272
PLASMAchicken <plasmachickenmc@gmail.com>
2020-05-22 03:20:14 +0200

    Pay return if number negative

--- src/BPEssentials/Commands/Pay/Pay.cs
+++ src/BPEssentials/Commands/Pay/Pay.cs
@@ -10,6 +10,11 @@ namespace BPEssentials.Commands
     {
         public void Invoke(ShPlayer player, ShPlayer target, int amount=1)
         {
+            if (0 >= amount)
+            {
+                player.TS("pay_error_negative");
+                return;
+            }
             if (player.MyMoneyCount() < amount)
             {
                 player.TS("you_dont_own", amount.ToString());
