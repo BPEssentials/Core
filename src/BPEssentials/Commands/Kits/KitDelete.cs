8215571b829189a1b8bd1af4282a7420eeb1b16d
PLASMAchicken <plasmachickenmc@gmail.com>
2020-09-06 15:19:58 +0200

    Added Kit + Warp delete

---
+++ src/BPEssentials/Commands/Kits/KitDelete.cs
@@ -0,0 +1,27 @@
+﻿using BPEssentials.Abstractions;
+using BPEssentials.ExtensionMethods;
+using BPEssentials.Utils;
+using BrokeProtocol.Entities;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace BPEssentials.Commands
+{
+    public class KitDelete : Command
+    {
+        public void Invoke(ShPlayer player, string kit)
+        {
+            var file = Path.Combine(Core.Instance.Paths.KitsFolder, $"{kit}.json");
+            if (!File.Exists(file))
+            {
+                player.TS("expFileHandler_error_notFound", player.T(Core.Instance.KitHandler.Name), kit);
+                player.TS("levenshteinSuggest", Core.Instance.KitHandler.List.OrderByDescending(x => LevenshteinDistance.CalculateSimilarity(x.Name, kit)).FirstOrDefault().Name);
+                return;
+            }
+
+            Core.Instance.KitHandler.DeleteExisting(kit);
+            player.TS("expFileHandler_deleted", player.T(Core.Instance.KitHandler.Name), kit);
+        }
+    }
+}
