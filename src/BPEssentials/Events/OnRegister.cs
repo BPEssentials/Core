905cc665548809580718e3274d33a8c644400b67
PLASMAchicken <plasmachickenmc@gmail.com>
2020-12-29 03:51:56 +0100

    Add back LimitNames ( was removed due to missing BPE EXTEND comments

--- src/BPEssentials/Events/OnRegister.cs
+++ src/BPEssentials/Events/OnRegister.cs
@@ -19,6 +19,11 @@ namespace BPEssentials.RegisteredEvents
                 svManager.RegisterFail(connectionData.connection, $"Your Username can only contain letters: A-Z a-z 0-9 _ -");
                 return false;
             }
+            if (Core.Instance.Settings.General.DisableAccountOverwrite)
+            {
+                svManager.RegisterFail(connectionData.connection, "This Name has already been registerd and this Server has disabled overwriting Accounts!");
+                return false;
+            }
             return true;
         }
     }
