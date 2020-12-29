89cbbfbe094950d2257616f053f36a8c692da133
PLASMAchicken <plasmachickenmc@gmail.com>
2020-12-29 03:00:16 +0100

    Added optional argument to /launch <player> <thrust>

--- src/BPEssentials/Commands/Misc/Launch.cs
+++ src/BPEssentials/Commands/Misc/Launch.cs
@@ -7,10 +7,10 @@ namespace BPEssentials.Commands
 {
     public class Launch : Command
     {
-        public void Invoke(ShPlayer player, ShPlayer target = null)
+        public void Invoke(ShPlayer player, ShPlayer target = null, float thrust = 6500f)
         {
             target = target ?? player;
-            target.svPlayer.SvForce(new UnityEngine.Vector3(0f, 6500f, 0f));
+            target.svPlayer.SvForce(new UnityEngine.Vector3(0f, thrust, 0f));
             target.TS("target_launched");
             player.TS("player_launched", target.username.CleanerMessage());
         }
