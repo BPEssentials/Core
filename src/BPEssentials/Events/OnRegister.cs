de5250918066feb7e58cb37e3a650576a34ab5d1
PLASMAchicken <plasmachickenmc@gmail.com>
2020-12-29 15:16:07 +0100

    added check if account is already registered

--- src/BPEssentials/Events/OnRegister.cs
+++ src/BPEssentials/Events/OnRegister.cs
@@ -19,7 +19,7 @@ namespace BPEssentials.RegisteredEvents
                 svManager.RegisterFail(connectionData.connection, $"Your Username can only contain letters: A-Z a-z 0-9 _ -");
                 return false;
             }
-            if (Core.Instance.Settings.General.DisableAccountOverwrite)
+            if (svManager.TryGetUserData(connectionData.username, out var user) && Core.Instance.Settings.General.DisableAccountOverwrite)
             {
                 svManager.RegisterFail(connectionData.connection, "This Name has already been registerd and this Server has disabled overwriting Accounts!");
                 return false;
