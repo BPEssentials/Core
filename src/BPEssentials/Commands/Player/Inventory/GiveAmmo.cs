a383de835df3587c904465cb06ba5c3fc10a7091
PLASMAchicken <plasmachickenmc@gmail.com>
2020-12-29 03:02:41 +0100

    Added /giveammo

---
+++ src/BPEssentials/Commands/Player/Inventory/GiveAmmo.cs
@@ -0,0 +1,27 @@
+﻿using BPEssentials.Abstractions;
+using BPEssentials.ExtensionMethods;
+using BPEssentials.Utils;
+
+using BrokeProtocol.Entities;
+using BrokeProtocol.Utility;
+using System.Linq;
+
+namespace BPEssentials.Commands
+{
+    public class GiveAmmo : Command
+    {
+        public void Invoke(ShPlayer player, int amount = 1, ShPlayer target = null)
+        {
+            target = target ?? player;
+
+            if (!target.curEquipable || !target.curEquipable.AmmoItem)
+            {
+                player.TS("player_giveammo_fail");
+            }
+
+            var item = target.curEquipable.AmmoItem;
+            target.TransferItem(DeltaInv.AddToMe, item.index, amount, true);
+            player.TS("player_give", target.username.CleanerMessage(), item.itemName, amount.ToString());
+        }
+    }
+}
