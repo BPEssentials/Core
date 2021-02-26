d9baff32dd10384d524de60179019d71438970ec
NongBenz <nongbenzgames@gmail.com>
2021-02-27 00:26:05 +0700

    Update to 1.12 Hotfix3 changes and match GameSource briefcase drop behavior

--- src/BPEssentials/Events/OnDropDead.cs
+++ src/BPEssentials/Events/OnDropDead.cs
@@ -1,11 +1,8 @@
-﻿using BPEssentials.ExtensionMethods;
-using BrokeProtocol.API;
+﻿using BrokeProtocol.API;
 using BrokeProtocol.Entities;
-using BrokeProtocol.Required;
 using BrokeProtocol.Utility;
-using BrokeProtocol.Utility.AI;
-using System;
 using System.Linq;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace BPEssentials.RegisteredEvents
@@ -15,20 +12,23 @@ namespace BPEssentials.RegisteredEvents
         private readonly int[] LicenseIDs = new[] { -700261193, 607710552, 499504400, 1695812550, -568534809 };
 
         [Target(GameSourceEvent.PlayerRemoveItemsDeath, ExecutionMode.Override)]
-        protected void OnRemoveItemsDeath(ShPlayer player)
+        protected void OnRemoveItemsDeath(ShPlayer player, bool dropItems)
         {
             // -- BPE EXTEND
-            if (BPEssentials.Core.Instance.Settings.KeptItemsOnDeath.KeepAllItemsOnDeath) { return; }
+            if (Core.Instance.Settings.KeptItemsOnDeath.KeepAllItemsOnDeath) { return; }
             // BPE EXTEND --
+
+            // List for removed items for dropping into briefcase
+            List<InventoryItem> removedItems = new List<InventoryItem>();
 
             // Allows players to keep items/rewards from job ranks
             foreach (InventoryItem myItem in player.myItems.Values.ToArray())
             {
                 // -- BPE EXTEND
-                if (BPEssentials.Core.Instance.Settings.KeptItemsOnDeath.KeptItemIds.Contains(myItem.item.index)) { continue; }
-                if (BPEssentials.Core.Instance.Settings.KeptItemsOnDeath.KeptItemNames.Contains(myItem.item.itemName)) { continue; }
-                if (BPEssentials.Core.Instance.Settings.KeptItemsOnDeath.KeepAllPhones && myItem.item is ShPhone) { continue; }
-                if (BPEssentials.Core.Instance.Settings.KeptItemsOnDeath.KeepAllLicenses && LicenseIDs.Contains(myItem.item.index)) { continue; }
+                if (Core.Instance.Settings.KeptItemsOnDeath.KeptItemIds.Contains(myItem.item.index)) { continue; }
+                if (Core.Instance.Settings.KeptItemsOnDeath.KeptItemNames.Contains(myItem.item.itemName)) { continue; }
+                if (Core.Instance.Settings.KeptItemsOnDeath.KeepAllPhones && myItem.item is ShPhone) { continue; }
+                if (Core.Instance.Settings.KeptItemsOnDeath.KeepAllLicenses && LicenseIDs.Contains(myItem.item.index)) { continue; }
 
                 // BPE EXTEND --
 
@@ -50,7 +50,39 @@ namespace BPEssentials.RegisteredEvents
                 // Remove everything except legal items currently worn
                 if (extra > 0 && (myItem.item.illegal || !(myItem.item is ShWearable w) || player.curWearables[(int)w.type].index != w.index))
                 {
+                    removedItems.Add(new InventoryItem(myItem.item, extra));
                     player.TransferItem(DeltaInv.RemoveFromMe, myItem.item.index, extra, true);
+                }
+            }
+
+            if (dropItems)
+            {
+                // Only drop items if attacker present, to prevent AI suicide item farming
+                if (Physics.Raycast(
+                    player.GetPosition + Vector3.up,
+                    Vector3.down,
+                    out RaycastHit hit,
+                    10f,
+                    MaskIndex.world))
+                {
+                    ShEntity briefcase = player.manager.svManager.AddNewEntity(
+                        player.manager.svManager.briefcasePrefabs.GetRandom(),
+                        player.GetPlace,
+                        hit.point,
+                        Quaternion.LookRotation(player.GetPositionT.forward),
+                        false);
+
+                    if (briefcase)
+                    {
+                        foreach (var invItem in removedItems)
+                        {
+                            if (Random.value < 0.8f)
+                            {
+                                invItem.count = Mathf.CeilToInt(invItem.count * Random.Range(0.05f, 0.3f));
+                                briefcase.myItems.Add(invItem.item.index, invItem);
+                            }
+                        }
+                    }
                 }
             }
         }
