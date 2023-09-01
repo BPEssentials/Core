414e4acf9762144c0958ce735551d366019d3d1f
PLASMAchicken <plasmachickenmc@gmail.com>
2023-09-02 00:43:08 +0200

    Create DestroyVehicle.cs

---
+++ src/BPEssentials/Commands/Vehicles/DestroyVehicle.cs
@@ -0,0 +1,21 @@
+﻿using BPEssentials.Abstractions;
+using BPEssentials.ExtensionMethods;
+using BrokeProtocol.Entities;
+
+namespace BPEssentials.Commands
+{
+    public class DestroyVehicle : BpeCommand
+    {
+        public void Invoke(ShPlayer player)
+        {
+            var vehicle = player.curMount as ShTransport;
+            if (vehicle == null)
+            {
+                player.TS("player_notInVehicle");
+                return;
+            }
+            vehicle.Destroy();
+            player.TS("player_vehicle_destroyed", vehicle.name);
+        }
+    }
+}
